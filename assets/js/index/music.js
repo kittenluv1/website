bff876035b4008ea2b14a87ca2b163c616afc088
kittenluv1 <ericahuang916@gmail.com>
2025-01-21 22:22:14 -0800

    updated portfolio, made tama shell customizable

--- assets/js/index/music.js
+++ assets/js/index/music.js
@@ -1,5 +1,5 @@
 
-//modal interactivity
+// MODAL INTERACTIVITY
 const modal = document.getElementsByClassName('modal')[0]; 
 document.getElementById('music').addEventListener('dblclick', toggleDisplay);
 modal.addEventListener('dblclick', toggleDisplay); 
@@ -12,28 +12,78 @@ function toggleDisplay() {
 	}
 }
 
-let isDragging = false; 
-let offsetX, offsetY; 
+let dragStart = false; 
+let isDragging = false;
+let offsetX, offsetY;
+let dragThreshold = 5;
+let startX, startY;
+
 modal.addEventListener('mousedown', function(e) {
-	isDragging = true; 
-	offsetX = e.clientX - modal.offsetLeft; 
-	offsetY = e.clientY - modal.offsetTop; 
-	document.body.style.cursor = 'grabbing';
-})
-document.addEventListener('mousemove', function(e) {
-	if (isDragging) {
-		let newLeft = e.clientX - offsetX; 
-		let newTop = e.clientY - offsetY; 
-		modal.style.left = `${newLeft}px`;
-		modal.style.top = `${newTop}px`; 
+	dragStart = true;
+    startX = e.clientX; 
+    startY = e.clientY;
+    offsetX = e.clientX - modal.offsetLeft;
+    offsetY = e.clientY - modal.offsetTop;
+    document.body.style.cursor = 'grabbing';
+});
+
+modal.addEventListener('mousemove', function(e) {
+    if (dragStart) {
+        // Check if the mouse moved enough to consider it a drag
+        let distX = Math.abs(e.clientX - startX);
+        let distY = Math.abs(e.clientY - startY);
+        if (distX > dragThreshold || distY > dragThreshold) {
+            isDragging = true;  // If movement exceeds threshold, start dragging
+        }
+    }
+
+    if (isDragging) {
+        let newLeft = e.clientX - offsetX;
+        let newTop = e.clientY - offsetY;
+        modal.style.left = `${newLeft}px`;
+        modal.style.top = `${newTop}px`;
+    }
+});
+
+modal.addEventListener('mouseup', (e) => {
+	dragStart = false; 
+    setTimeout(() => {
+        isDragging = false;
+        document.body.style.cursor = 'default';
+    }, 0); // Delay the reset by a small amount
+});
+
+// tama shells
+const screenDiv = document.getElementById('screen-div');
+shellImage = document.getElementById('tama-shell'); 
+screenDiv.addEventListener('click', (event) => { event.stopPropagation(); });
+modal.addEventListener('click', (e) => {
+	if (isDragging) return;
+	// change shell images
+	let src = shellImage.src.split('/').pop();
+	if (src === "2.png") {
+		shellImage.src = "assets/images/index/shells/3.png";
+		modal.style.gridTemplateRows = "1.3fr 1.5fr .2fr 4.9fr .9fr 1.5fr 1.6fr";
+		modal.style.gridTempateColumns = "2.4fr .6fr .4fr .5fr 1fr .6fr .4fr .6fr 2.2fr"
+	}
+	else if (src === "3.png") {
+		shellImage.src = "assets/images/index/shells/4.png";
+		modal.style.gridTemplateRows = ".3fr 2.9fr .2fr 4.8fr .8fr 1.5fr 1.4fr";
+		modal.style.gridTemplateColumns = "2.3fr .6fr .4fr .5fr 1fr .6fr .4fr .6fr 2.3fr";
+	}
+	else if (src === "4.png") {
+		shellImage.src = "assets/images/index/shells/1.png"
+		modal.style.gridTemplateRows = "1.3fr 1.7fr .2fr 4.7fr 1.5fr 1.5fr 1fr"; 
+		modal.style.gridTemplateColumns = "2.5fr .6fr .4fr .5fr 1fr .6fr .4fr .6fr 2.4fr"; 
+	}
+	else {
+		shellImage.src = "assets/images/index/shells/2.png";
+		modal.style.gridTemplateRows = "1.5fr 1.7fr .4fr 5fr .7fr 1.5fr 1.2fr"; 
+		modal.style.gridTemplateColumns = "2.4fr .5fr .4fr .6fr .9fr .6fr .4fr .5fr 2.2fr"; 
 	}
 })
-document.addEventListener('mouseup', () => {
-	isDragging = false; 
-	document.body.style.cursor = 'default';
-})
 
-// tama screens
+// TAMA SCREENS
 const instructions = document.getElementById('instructions'); 
 const game = document.getElementById('tama-game');
 let screenImage = document.getElementById('music-image');
@@ -95,13 +145,14 @@ modal.addEventListener('keyup', (e) => {
 	isKeyPress = false; 
 })
 
-// images
+// IMAGES
 let imageIndex = 0;
 fetch('assets/json/index/images.json')
 	.then(response => response.json())
 	.then(images => {
 		screenImage.src = images[imageIndex]; 	//load first image
 		screenImage.addEventListener('click', () => {
+			if (isDragging) return;
 			screenImage.src = images[changeImage()];
 		});
 		function changeImage() {
@@ -110,7 +161,7 @@ fetch('assets/json/index/images.json')
 		}
 	})
  
-// audio player functionality
+// AUDIO
 let songIndex = 0;
 const songs = [];
 const audio = document.getElementById('audio');
@@ -162,8 +213,11 @@ const modalButtons = [heartButton, prevButton, playButton, nextButton];
 modalButtons.forEach(button => {
 	button.addEventListener('mouseenter', () => {
 		hover.play(); 
-	})
+	});
 	button.addEventListener('click', () => {
 		click.play(); 
-	})
+	});
+	button.addEventListener('click', (event) => {
+		event.stopPropagation(); 
+	});
 })
