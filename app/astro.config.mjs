0cd84292e740dc9e1d3b9f2c059f24bcd4397607
kittenluv1 <ericahuang916@gmail.com>
2026-08-24 12:05:52 -0700

    add tailwind

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://bunnyluv1.com",
+
   redirects: {
     "/blog": "/blog/2026",
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
